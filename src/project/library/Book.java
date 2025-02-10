c4c4dafb5f2100ff13583f96ecd0ca7cd0f3bb9f
Caleb <caleb.rich2004@gmail.com>
2025-02-10 17:05:11 -0600

    Added getter functions and all Book constructors; Initialized LibraryApp to allow for testing of new functions

--- src/project/library/Book.java
+++ src/project/library/Book.java
@@ -1,16 +1,48 @@
 package project.library;
 
 public class Book {
-	private String title;
+    private String title;
     private String author;
     private String ISBN;
     private double price;
     
+    //Constructor for Book with no information
 	public Book() {
 		this.author = "unknown";
         this.title = "unknown";
         this.ISBN = "unknown";
         this.price = 0.0;
+	}
+	//Constructor for book given information
+	public Book(String title, String author, String ISBN, double price) {
+		this.author = author;
+		this.title = title;
+		this.ISBN = ISBN;
+		this.price = price;
+	}
+	//Constructor for Book given a book to copy
+	public Book(Book book) {
+		this.title = book.title;
+		this.author = book.author;
+		this.ISBN = book.ISBN;
+		this.price = book.price;
+	}
+	
+	//Getter and setter functions
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public String getAuthor() {
+		return this.author;
+	}
+	
+	public String getISBN() {
+		return this.ISBN;
+	}
+	
+	public double getPrice() {
+		return this.price;
 	}
 
 }
--- src/project/library/LibraryApp.java
+++ src/project/library/LibraryApp.java
@@ -1,8 +1,13 @@
 package project.library;
 
+import project.library.Book;
+
 public class LibraryApp {
 	public static void main (String[] args) {
+		//For now, use this as a function tester. We will put it to specs once Book and Library are fully functional.
+		Book book1 = new Book("Cat in the Hat", "Dr. Seuss", "123456789", 7.95);
 		
+		System.out.println("Title: "  + book1.getTitle() + " Author: " + book1.getAuthor() + " ISBN: " + book1.getISBN() + " Price: " + book1.getPrice());
 	}
 
 }
